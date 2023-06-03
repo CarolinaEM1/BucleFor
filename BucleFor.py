5203a221be35fe76002babfb358ae7ede6b221fe
Carolina Martinez <carolinamtz091@gmail.com>
2023-06-02 23:09:25 -0600

    primera version

---
+++ BucleFor.py
@@ -0,0 +1,16 @@
+#Bucle for
+
+coleccion = [1,2,3,4,5,"Carolina"]
+
+for i in coleccion:
+    print("Hola Mundo")
+
+for i in coleccion:
+    print(i)
+
+diccionario = {"Carolina":"20","Mariana":"24","Daniela":"29"}
+
+for clave,valor in diccionario.items():
+    print(f"{clave} -> {valor}")
+
+#Carolina EM
